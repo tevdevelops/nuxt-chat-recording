918640f16c61b3d99b0c53da847adaf596b33fd2
Tevin Rivera <tevin@bradsdeals.com>
2025-09-17 18:00:22 -0600

    - create some Chat types for our new useChat composable - create mvp of useChat composable that returns chat, messages, and a   sendMessage func to add to the messages array

---
+++ app/composables/useChat.ts
@@ -0,0 +1,29 @@
+import type { Chat, ChatMessage } from '../types'
+import { MOCK_CHAT } from './mockData'
+
+export default function useChat() {
+  const chat = ref<Chat>(MOCK_CHAT)
+  const messages = computed<ChatMessage[]>(() => {
+    return chat.value.messages
+  })
+
+  function createMessage(message: string, role: ChatMessage['role']) {
+    const id = messages.value.length.toString()
+
+    return {
+      id,
+      role,
+      content: message,
+    }
+  }
+
+  function sendMessage(message: string) {
+    messages.value.push(createMessage(message, 'user'))
+
+    setTimeout(() => {
+      messages.value.push(createMessage(`You said: ${message}`, 'assistant'))
+    }, 200)
+  }
+
+  return { chat, messages, sendMessage }
+}
